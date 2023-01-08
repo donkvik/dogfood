c758c0ae86df4dbc43acbcc612dfc1dbf7ac2a64
donkvik <hahyrningur.o@gmail.com>
2023-01-08 16:35:42 +0300

    hw

---
+++ src/components/Form/form.jsx
@@ -0,0 +1,15 @@
+import s from './index.module.css';
+import './style.css';
+
+function Form({ title, handleFormSubmit, children }) {
+
+    return (
+        <form className={s.form} onSubmit={handleFormSubmit}>
+            <h1 className={s.title}>{title}</h1>
+            {children}
+
+        </form>
+    );
+};
+
+export default Form;
---
+++ src/utils/api.js
@@ -0,0 +1,69 @@
+const onResponce = (res) => {
+    return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
+}
+
+class Api {
+    constructor({baseUrl, headers}){
+        this._headers = headers;
+        this._baseUrl= baseUrl;
+    }
+
+    getProductList() {
+        return fetch(`${this._baseUrl}/products`, {
+            headers: this._headers
+        }).then(onResponce)
+    }
+
+    getUserInfo() {
+        return fetch(`${this._baseUrl}/users/me`, {
+            headers: this._headers
+        }).then(onResponce)
+    }
+
+    getProductById(idProduct) {
+        return fetch(`${this._baseUrl}/products/${idProduct}`, {
+            headers: this._headers
+        }).then(onResponce)
+    }
+
+    setUserInfo(dataUser) {
+        return fetch(`${this._baseUrl}/users/me`, {
+            method: 'PATCH',
+            headers: this._headers,
+            body: JSON.stringify(dataUser)
+        }).then(onResponce)
+    }
+
+    createReviewProduct(productId, reviewData) {
+        return fetch(`${this._baseUrl}/products/review/${productId}`, {
+            method: 'POST',
+            headers: this._headers,
+            body: JSON.stringify(reviewData)
+        }).then(onResponce)
+    }
+
+    search(searchQuery) {
+        return fetch(`${this._baseUrl}/products/search?query=${searchQuery}`, {
+            headers: this._headers
+        }).then(onResponce)
+    }
+
+    changeLikeProduct(productId, isLike) {
+        return fetch(`${this._baseUrl}/products/likes/${productId}`, {
+            method: isLike ? "DELETE" : "PUT",
+            headers: this._headers
+        }).then(onResponce)
+    }
+}
+
+const config = {
+    baseUrl: 'https://api.react-learning.ru',
+    headers: {
+        'content-type': 'application/json',
+        Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZhNTEwNjU5Yjk4YjAzOGY3NzljZmIiLCJncm91cCI6Imdyb3VwLTciLCJpYXQiOjE2Njc5MTE5NDYsImV4cCI6MTY5OTQ0Nzk0Nn0.lBWDveGspmJ0TXFi5t5JBVqk8neFLsNbxl2YtJihVno'
+    }
+}
+
+const api = new Api(config);
+
+export default api;
