47818214bd743c45bf671eba9a71a9b0aac9ed71
donkvik <hahyrningur.o@gmail.com>
2023-01-12 15:41:07 +0300

    del

--- src/pages/NotFoundPage/not-found-page.jsx
+++
@@ -1,9 +0,0 @@
-import { NotFound } from '../../components/NotFound/NotFound'
-
-export const NotFoundPage = () => {
-    return (
-      <>
-      <NotFound  title='Страница не найдена' buttonText='На главную'/>
-      </>
-    )
-}
